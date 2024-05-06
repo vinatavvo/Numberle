394ea890b16b27866944f09c4242c6053c6c0b8e
varunj25 <vjonna@terpmail.umd.edu>
2024-05-06 18:27:43 -0400

    started firebase

--- app/src/main/java/com/example/cmsc436groupproject/LoginView.kt
+++ app/src/main/java/com/example/cmsc436groupproject/LoginView.kt
@@ -2,12 +2,22 @@ package com.example.cmsc436groupproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseReference
+
 class LoginView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var firebase: FirebaseDatabase = FirebaseDatabase.getInstance()
+        var reference: DatabaseReference = firebase.getReference("email")
+
+        reference.setValue("test")
+
+
     }
 
-    // firebase code
+
 
 }

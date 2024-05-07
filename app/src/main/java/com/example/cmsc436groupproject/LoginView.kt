086995e28cfc0d5cccf25c4863754cb99c9e2e9c
varunj25 <vjonna@terpmail.umd.edu>
2024-05-07 14:21:33 -0400

    more stuff

--- app/src/main/java/com/example/cmsc436groupproject/LoginView.kt
+++ app/src/main/java/com/example/cmsc436groupproject/LoginView.kt
@@ -1,22 +1,40 @@
 package com.example.cmsc436groupproject
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.IgnoreExtraProperties
+import com.google.firebase.database.ValueEventListener
+import kotlin.properties.Delegates
 
 class LoginView : AppCompatActivity() {
+    lateinit var firebase:FirebaseDatabase
+    lateinit var reference: DatabaseReference
+    lateinit var email:String
+    var score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        var firebase: FirebaseDatabase = FirebaseDatabase.getInstance()
-        var reference: DatabaseReference = firebase.getReference("email")
+        firebase = FirebaseDatabase.getInstance()
+        reference = firebase.getReference("emails")
 
-        reference.setValue("test")
+
 
 
     }
+
+    fun play(v: View){
+        var email = findViewById<EditText>(R.id.editTextEmail).toString()
+
+    }
+
 
 
 
--- app/src/main/java/com/example/cmsc436groupproject/MainActivity.kt
+++ app/src/main/java/com/example/cmsc436groupproject/MainActivity.kt
@@ -1,17 +1,40 @@
 package com.example.cmsc436groupproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.IgnoreExtraProperties
+import com.google.firebase.database.ValueEventListener
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var gameView: GameView
     private var level: Int = 5
+    lateinit var firebase:FirebaseDatabase
+    lateinit var reference: DatabaseReference
+    lateinit var email:String
+    var score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-//        gameView = GameView(this, level)
-//        setContentView(gameView)
+        firebase = FirebaseDatabase.getInstance()
+        reference = firebase.getReference("emails")
+
+    }
+
+    fun play(v: View){
+        var email = findViewById<EditText>(R.id.editTextEmail).toString()
+
+
+        gameView = GameView(this, level)
+        setContentView(gameView)
     }
 }
